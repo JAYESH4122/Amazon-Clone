bbe31084cd2120e336a7a466ad30b0e9f0cd90da
JAYESH4122 <jayeshpj343@gmail.com>
2025-06-09 18:50:36 +0530

    initial commit

---
+++ common.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const app = document.getElementById("app");
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+    if (isMobile) {
+        loadMobileVersion();
+    } else {
+        loadDesktopVersion();
+    }
+
+    window.addEventListener("resize", function() {
+        const currentIsMobile = window.matchMedia("(max-width: 768px)").matches;
+        
+        if (currentIsMobile !== isMobile) {
+            location.reload();
+        }
+    });
+});
+
+function loadMobileVersion() {
+    fetch("mobile/mobile.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("app").innerHTML = html;
+            
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "mobile/mobile.css";
+            document.head.appendChild(link);
+        });
+}
+
+
+function loadDesktopVersion() {
+    fetch("desktop-new/desku.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("app").innerHTML = html;
+            
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "desktop/desktop.css";
+            document.head.appendChild(link);
+        });
+}
+
